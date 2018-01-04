df200ab527df16c2a15a90baa10175775a26233b
lyken17 <lykensyu@gmail.com>
2018-01-03 17:41:33 -0800

    Test Trainer functionality.

--- lith/trainer.py
+++ lith/trainer.py
@@ -1,5 +1,6 @@
 import os.path as osp
 import logging
+import collections
 from collections import OrderedDict
 
 import torch
@@ -12,82 +13,13 @@ import math
 import numpy as np
 import torch
 
-
-class Metric(object):
-    def __init__(self):
-        pass
-
-    @property
-    def name(self):
-        return self.__class__.__name__
-
-    def torchlize(func):
-        # preprocess the args from numpy into torch tensor
-        def convert(*args, **kwargs):
-            new_args = []
-            for idx, each in enumerate(args):
-                if isinstance(each, np.ndarray):
-                    each = torch.from_numpy(each)
-                new_args.append(each)
-            args = tuple(new_args)
-
-            for key, value in kwargs:
-                if isinstance(value, np.ndarray):
-                    kwargs[key] = torch.from_numpy(value)
-            return func(*args, **kwargs)
-
-        return convert
-
-
-class Compose(Metric):
-    def __init__(self, metrics):
-        super(Compose, self).__init__()
-        if not isinstance(metrics, list):
-            self.metrics = metrics
-
-    def __call__(self, output, target):
-        result = OrderedDict()
-        for m in self.metrics:
-            key = m.name
-            value = m(output, target)
-            result[key] = value
-        return result
-
-
-class TopKAccuracy(Metric):
-    def __init__(self, k):
-        super(TopKAccuracy, self).__init__()
-        self.k = k
-
-    @property
-    def name(self):
-        return "Top-%d-Accuracy" % self.k
-
-    @Metric.torchlize
-    def __call__(self, output, target):
-        """
-        :param output: *xC
-        :param target: *x1
-        :return:
-        """
-        assert output.dim() == target.dim(), \
-            '''To compute TopK, `output` must have same shape as `target` '''
-        sorted, indices = torch.topk(output, self.k, output.dim() - 1)
-        comp = indices - target
-        correct = comp.eq(0).cpu().sum()
-        # print(indices.size(), target.size(), correct, output.size(0))
-        return correct / output.size(0)
-
-
-class Accuracy(TopKAccuracy):
-    def __init__(self):
-        super(Accuracy, self).__init__(k=1)
+from .metric import Compose, Accuracy, Error
 
 
 class Trainer(object):
-    def __init__(self, model, train_loader, valid_loader,
+    def __init__(self, model, train_loader, valid_loader, optimizer,
                  start_epoch=0, total_epoch=150,
-                 criterion=nn.CrossEntropyLoss(), eval_criterion=Compose(Accuracy()),
+                 criterion=nn.CrossEntropyLoss(), eval_criterion=Compose(Error()),
                  scheduler=None, root=".", name="base"):
         # init variables
         self.epoch = start_epoch
@@ -107,17 +39,22 @@ class Trainer(object):
 
         # init by assignment
         self.model = model
-        self.optimizer = None
+        self.optimizer = optimizer
         self.epoch = self.start_epoch = start_epoch
         self.total_epochs = total_epoch
+        self.best_error = 100
 
         self.criterion = criterion
 
         # fb.resnet.torch scheduler for CIFAR
         self.scheduler = scheduler
         if scheduler is None:
-            self.scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=self.optimizer,
-                milestones=[int(self.total_epochs * 0.5), int(self.total_epochs * 0.75)], gamma=0.1)
+            self.scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                optimizer=self.optimizer,
+                milestones=[int(self.total_epochs * 0.5),
+                            int(self.total_epochs * 0.75)],
+                gamma=0.1
+            )
 
         # other variables
         logging.basicConfig(level=logging.INFO,
@@ -137,32 +74,34 @@ class Trainer(object):
         while self.epoch <= self.total_epochs:
             self.scheduler.step(self.epoch)
             self.train(self.train_loader, self.optimizer, self.epoch)
-            self.valid(self.valid_loader, self.epoch)
-            self.snapshot()
+            loss, error = self.valid(self.valid_loader, self.epoch)
+            self.snapshot(error)
             self.epoch += 1
 
     def log_info(self, type, epoch, progress, output, target, loss=None):
-        msg = "%s: %d [%d/%d]" % (type, epoch, progress[0], progress[1])
+        msg = "%s: %d [%d/%d](%.2f) " % (type, epoch, progress[0], progress[1], progress[0] / progress[1] * 100)
         if loss is not None:
-            msg += "{}: {}".format("Loss", loss.data.cpu())
+            msg += "{}: {:.4f}\t".format("Loss", loss.data.cpu()[0])
         length = len(msg)
 
         evaluation = self.eval_criterion(output, target)
         for key, value in evaluation.items():
-            current = "{}: {}".format(key, value)
+            current = "{}: {:.4f}\t".format(key, value)
             length += len(current)
             if length > 80:
                 self.logger(msg)
                 msg = ""
             msg += current
             length = len(msg)
+        self.logger(msg)
+        return evaluation
 
     # train one epoch
     def train(self, train_loader, optimizer, epoch):
         self.model.train()
         n_samples = len(train_loader.dataset)
         n_batchs = len(train_loader)
-        for batch_idx, (input, target) in train_loader:
+        for batch_idx, (input, target) in enumerate(train_loader):
             if self.use_cuda:
                 input, target = input.cuda(), target.cuda()
             input, target = Variable(input), Variable(target)
@@ -173,46 +112,57 @@ class Trainer(object):
             loss.backward()
             optimizer.step()
             # evaluation
-            self.log_info("train", epoch, [batch_idx, n_batchs], output, target, loss)
+            self.log_info("Train", epoch, [batch_idx, n_batchs], output, target, loss)
             # log information
 
-    def valid(self, valid_loader , epoch):
+    def valid(self, valid_loader, epoch):
         self.model.eval()
         n_samples = len(valid_loader.dataset)
         n_batchs = len(valid_loader)
-        for batch_idx, (input, target) in valid_loader:
+
+        total_error = 0.0
+        total_loss = 0.0
+
+        for batch_idx, (input, target) in enumerate(valid_loader):
             if self.use_cuda:
                 input, target = input.cuda(), target.cuda()
             input, target = Variable(input), Variable(target)
             output = self.model(input)
             loss = self.criterion(output, target)
+
             # evaluation
-            self.log_info("valid", epoch, [batch_idx, n_batchs], output, target)
-            # log information
-        return
+            evaluation = self.log_info("Valid", epoch, [batch_idx, n_batchs], output, target, loss)
+            batch_size = input.size(0)
+            total_error += evaluation["Error"] * batch_size
+            total_loss += loss * batch_size
+
+        return total_loss / n_samples, total_error / n_samples
 
     def evaluate(self, test_loader):
+        raise NotImplementedError
         self.model.eval()
         n_samples = len(test_loader.dataset)
-        for batch_idx, (input, target) in test_loader:
+        for batch_idx, (input, target) in enumerate(test_loader):
             if self.use_cuda:
                 input, target = input.cuda(), target.cuda()
             input, target = Variable(input), Variable(target)
             output = self.model(input)
-            loss = self.criterion(output, target)
+            evaluation = self.eval_criterion(output, target)
 
     def serialize(self):
         raise NotImplementedError
 
-    def snapshot(self, is_best, error):
+    def snapshot(self,  error):
+        is_best = error < self.best_error
         if is_best:
+            self.best_error = error
             torch.save({
                 "model": self.model,
                 "optimizer": self.optimizer,
                 "best": is_best,
                 "error": error,
                 "epoch": self.epoch
-            }, osp.join(self.root, "model-best.t7"))
+            }, osp.join(self.root, self.name, "model-best.t7"))
 
         torch.save({
             "model": self.model,
@@ -220,8 +170,7 @@ class Trainer(object):
             "best": is_best,
             "error": error,
             "epoch": self.epoch
-        }, osp.join(self.root, "model-latest.t7"))
+        }, osp.join(self.root, self.name, "model-latest.t7"))
 
     def resume(self):
         pass
-
--- test/metric_test.py
+++ test/metric_test.py
@@ -3,16 +3,11 @@ import numpy as np
 
 from lith import metric
 
-m = metric.MeanIoU()
+m = metric.Error()
 
-output = torch.randn([10, 3, 32, 32])
-noise = torch.randn([10, 3, 32, 32]) / 5
-
-target = torch.randn([10, 1, 32, 32])
+output = torch.randn([64, 5])
+noise = torch.randn([64, 5])
 pred = (output + noise).max(1)[1]
-# pred = pred.view(pred.size(0), 1, pred.size(1), pred.size(2))
 
-# print(pred.size())
-# print(m(output, pred))
-
-print(m.name)
+res = m(output, pred)
+print(m.name, res, type(res))
